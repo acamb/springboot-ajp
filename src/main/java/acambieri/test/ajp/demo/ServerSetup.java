f7f35b639f5512defb696408957199ef3a39f003
andrea AC <andrea.cambieri@spaceinformatica.com>
2018-10-09 09:44:07 +0200

    spring boot application with ajp enabled.

---
+++ src/main/java/acambieri/test/ajp/demo/ServerSetup.java
@@ -0,0 +1,33 @@
+package acambieri.test.ajp.demo;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerSetup {
+
+    @Value("${ajp.port}")
+    int ajpPort;
+
+    @Value("${ajp.enabled}")
+    boolean ajpEnabled;
+
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        if (ajpEnabled) {
+            Connector ajpConnector = new Connector("AJP/1.3");
+            ajpConnector.setPort(ajpPort);
+            ajpConnector.setSecure(false);
+            ajpConnector.setScheme("http");
+            ajpConnector.setAllowTrace(false);
+            tomcat.addAdditionalTomcatConnectors(ajpConnector);
+        }
+        return tomcat;
+    }
+}
